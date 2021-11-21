8e41845664c3fc358750656f9b9aa4337884c8cb
brian <fantasyz7@naver.com>
2021-11-21 23:51:07 +0900

    participation ether amount modified to easy testing.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # squid_game_type1
+
 The game with random rewards
+
+Contract deployed in Ropsten Testnet Network.
+0x7c1cfb31B906157c690e595eE6f9D793F265b447
--- main.sol
+++ main.sol
@@ -1,31 +1,40 @@
 //SPDX-License-Identifier GPL-30
-pragma solidity >= 0.7.0 < 0.9.0;
-contract GambleGame{
+pragma solidity >=0.7.0 <0.9.0;
+
+contract GambleGame {
     event WhoPaid(address indexed sender, uint256 payment);
-    
+
     address owner;
-    mapping (uint256=>mapping(address=>bool)) paidMemberList;
-    uint256 round =1;
-    
-    constructor(){
+    mapping(uint256 => mapping(address => bool)) paidMemberList;
+    uint256 round = 1;
+
+    constructor() {
         owner = msg.sender;
     }
-    receive() external payable{
-        require(msg.value==1 ether, "Only 1 ether indeed");
-        require(paidMemberList[round][msg.sender]==false,"Every player can play just once each game");
-        paidMemberList[round][msg.sender]=true;
+
+    receive() external payable {
+        require(msg.value == 0.1 ether, "Only 0.1 ether indeed");
+        require(
+            paidMemberList[round][msg.sender] == false,
+            "Every player can play just once each game"
+        );
+        paidMemberList[round][msg.sender] = true;
         emit WhoPaid(msg.sender, msg.value);
-        if(address(this).balance==3 ether){
-            (bool sent,)=payable(msg.sender).call{value:address(this).balance}("");
-            require(sent,"Fail");
+        if (address(this).balance == 0.3 ether) {
+            (bool sent, ) = payable(msg.sender).call{
+                value: address(this).balance
+            }("");
+            require(sent, "Fail");
             round++;
         }
     }
-    function checkRound() public view returns(uint256){
+
+    function checkRound() public view returns (uint256) {
         return round;
     }
-    function checkValue() public view returns(uint256){
-        require(owner==msg.sender, "only owner can access");
+
+    function checkValue() public view returns (uint256) {
+        require(owner == msg.sender, "only owner can access");
         return address(this).balance;
     }
 }
